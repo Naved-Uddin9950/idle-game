69cb19560227ccef893b6a3ec3ac020d0dc08c9e
Naved Uddin <naveduddin83@gmail.com>
2024-04-13 11:53:43 +0530

    back button tag

--- src/views/main.js
+++ src/views/main.js
@@ -1,6 +1,7 @@
 export const mainView = () => {
     return `
     <div class='main-container'>
+        <back><h1>←</h1></back>
         <settings><h1>⚙</h1></settings>
         <play>Play</play>
     </div>
