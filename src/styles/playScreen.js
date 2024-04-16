3c7de0ee90cc1ffc9d6f1bf03e130d4a92de19d3
Naved Uddin <naveduddin83@gmail.com>
2024-04-16 14:05:29 +0530

    playScreen styles converted to js type

---
+++ src/styles/playScreen.js
@@ -0,0 +1,49 @@
+export const playStyles = () => {
+    const styleElement = document.createElement('style');
+    
+    const cssStyles = `
+        .game-screen {
+            position: relative;
+            width: 100%;
+            height: 100vh;
+            background-color: #f0f0f0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+    
+        .main-character {
+            position: absolute;
+            bottom: 0;
+            width: 200px;
+            height: auto;
+        }
+    
+        .gold-display {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            font-size: 18px;
+        }
+    
+        .icons {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            display: flex;
+            justify-content: flex-end;
+        }
+    
+        .icon {
+            width: 40px;
+            height: 40px;
+            margin-left: 10px;
+            cursor: pointer;
+        }
+    `;
+    
+    styleElement.textContent = cssStyles;
+    
+    document.head.appendChild(styleElement);
+}
--- src/views/play.js
+++ src/views/play.js
@@ -1,8 +1,16 @@
+import { playStyles } from "../styles/playScreen.js";
+
 export const playView = () => {
+    playStyles();
     return `
         <div class="game-screen">
-            <img src="../public/images/player.gif" alt="Main Character" class="main-character" id="main-character">
             <div class="gold-display">Gold: <span id="gold-count">0</span></div>
+            <div class="icons">
+                <p class="icon" id="settings-icon">⚙️</p>
+                <p class="icon" id="achievements-icon">&#127942;</p>
+                <p class="icon" id="shop-icon">&#128722;</p>
+            </div>
+            <img src="../public/images/player.gif" alt="Main Character" class="main-character" id="main-character">
             <div class="monster" id="monster"><img src="../public/images/eye_monster.webp" alt="monster"></div>
         </div>
     `;
