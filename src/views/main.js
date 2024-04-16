212de29fa3cdf16a090bee7af8d967f4d11cec5a
Naved Uddin <naveduddin83@gmail.com>
2024-04-16 12:09:27 +0530

    changed resources to gold

--- src/views/main.js
+++ src/views/main.js
@@ -14,7 +14,7 @@ export const mainView = () => {
             <div class="title">Idle Tapper Game</div>
             <img src="../public/images/player.gif" alt="Main Character" class="main-character">
             <play>Play</play>
-            <div class="resource-display">Resources: <span id="resource-count">0</span></div>
+            <div class="resource-display">Gold: <span id="resource-count">0</span></div>
             <div class="icons">
                 <p class="icon" id="settings-icon">⚙️</p>
                 <p class="icon" id="achievements-icon">&#127942;</p>
