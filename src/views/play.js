707b4ca97ea7c98de9fc7928be3a6c8e2e31aa33
Naved Uddin <naveduddin83@gmail.com>
2024-04-16 12:32:49 +0530

    recreated play view

--- src/views/play.js
+++ src/views/play.js
@@ -1,9 +1,9 @@
 export const playView = () => {
     return `
-    <div class='main-container'>
-        <back><h1>←</h1></back>
-        <settings><h1>⚙</h1></settings>
-        <div class='game-container'>hi</div>
-    </div>
+        <div class="game-screen">
+            <img src="../public/images/player.gif" alt="Main Character" class="main-character" id="main-character">
+            <div class="resource-display">Gold: <span id="gold-count">0</span></div>
+            <div class="monster" id="monster">Monster</div>
+        </div>
     `;
 }
