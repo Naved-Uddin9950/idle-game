4034e94446a0464d9d394c3cb5a0730105388a6a
Naved Uddin <naveduddin83@gmail.com>
2024-04-16 12:49:51 +0530

    bug fixes in gold controller

--- controllers/gold.js
+++ controllers/gold.js
@@ -1,11 +1,16 @@
+import { useState } from "../utils/useState";
+import { setState } from "../utils/setState";
+
 export const updateGold = (goldCoins) => {
   const goldDisplay = document.getElementById('gold-count');
+  setState('gold', goldCoins);
   goldDisplay.textContent = goldCoins;
 }
 
 export const getGold = () => {
   document.getElementById('main-character').addEventListener('click', () => {
+    let goldCount = Number (useState('gold')) ?? 0;
     goldCount++;
-    updateGoldDisplay();
+    updateGold();
   });
 }
