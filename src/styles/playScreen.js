1870fe8656a0958a3749288a5184f1e156629394
Naved Uddin <naveduddin83@gmail.com>
2024-04-16 14:24:49 +0530

    multiple stylesheet adding issue solve

--- src/styles/playScreen.js
+++ src/styles/playScreen.js
@@ -1,49 +1,54 @@
 export const playStyles = () => {
-    const styleElement = document.createElement('style');
+    const temp = document.getElementById('playScreenStyles');
 
-    const cssStyles = `
-        .game-screen {
-            position: relative;
-            width: 100%;
-            height: 100vh;
-            background-color: #f0f0f0;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-        }
+    if(!temp) {
+        const styleElement = document.createElement('style');
     
-        .main-character {
-            position: absolute;
-            bottom: 0;
-            width: 200px;
-            height: auto;
-        }
+        const cssStyles = `
+            .game-screen {
+                position: relative;
+                width: 100%;
+                height: 100vh;
+                background-color: #f0f0f0;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+            }
+        
+            .main-character {
+                position: absolute;
+                bottom: 0;
+                width: 200px;
+                height: auto;
+            }
+        
+            .gold-display {
+                position: absolute;
+                top: 10px;
+                left: 50px;
+                font-size: 18px;
+            }
+        
+            .icons {
+                position: absolute;
+                top: 10px;
+                right: 10px;
+                display: flex;
+                justify-content: flex-end;
+            }
+        
+            .icon {
+                width: 40px;
+                height: 40px;
+                margin-left: 10px;
+                cursor: pointer;
+            }
+        `;
     
-        .gold-display {
-            position: absolute;
-            top: 10px;
-            left: 50px;
-            font-size: 18px;
-        }
+        styleElement.textContent = cssStyles;
+        styleElement.id = 'playScreenStyles';
     
-        .icons {
-            position: absolute;
-            top: 10px;
-            right: 10px;
-            display: flex;
-            justify-content: flex-end;
-        }
-    
-        .icon {
-            width: 40px;
-            height: 40px;
-            margin-left: 10px;
-            cursor: pointer;
-        }
-    `;
-
-    styleElement.textContent = cssStyles;
-
-    document.head.appendChild(styleElement);
+        document.head.appendChild(styleElement);
+    }
 }
