dc5811f0276bfb3a413738391d12ab5e670e2a04
Naved Uddin <naveduddin83@gmail.com>
2024-04-16 13:45:54 +0530

    added monster in play screen

--- src/views/play.js
+++ src/views/play.js
@@ -3,7 +3,7 @@ export const playView = () => {
         <div class="game-screen">
             <img src="../public/images/player.gif" alt="Main Character" class="main-character" id="main-character">
             <div class="gold-display">Gold: <span id="gold-count">0</span></div>
-            <div class="monster" id="monster">Monster</div>
+            <div class="monster" id="monster"><img src="../public/images/eye_monster.webp" alt="monster"></div>
         </div>
     `;
 }
