d614c02bda0c54bb7a15244584fef8894b61c49e
Naved Uddin <naveduddin83@gmail.com>
2024-04-17 11:32:38 +0530

    status view

---
+++ src/views/status.js
@@ -0,0 +1,11 @@
+export const statusView = () => {
+    return `
+        <div id="userModal" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <h2>User Profile</h2>
+                <div id="userInfo"></div>
+            </div>
+        </div>
+    `;
+}
