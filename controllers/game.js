2bd2d0451eea2a2388ef0d670837162df568779c
Naved Uddin <naveduddin83@gmail.com>
2024-04-17 09:44:48 +0530

    separated main controller

--- controllers/game.js
+++ controllers/game.js
@@ -1,34 +1,20 @@
-import { mainView } from '../src/views/main.js';
 import { playView } from '../src/views/play.js';
-import { errorHandler } from '../utils/errorHandler.js';
 import { setState } from '../utils/setState.js';
 import { useState } from '../utils/useState.js';
 import { useMiddleware } from '../utils/useMiddleware.js';
 import { back } from '../utils/backButton.js';
-import { getGold, updateGold } from './gold.js';
 // import { monsters } from './monsters.js';
 import { Player } from './player.js';
 
-const gameWrapper = document.getElementById('game-wrapper');
+import { getGold, updateGold } from './gold.js';
+import { errorHandler } from '../utils/errorHandler.js';
+
+import { main } from './main.js';
+useMiddleware(main);
 
 const player = new Player('Shinigami', 'Male', 100, 10, 1, 0);
 const playerData = JSON.parse(useState('player')) || player;
 
-
-
-// Initialiazes main screen
-export const main = () => {
-    const mainScreen = mainView();
-    try {
-        
-        gameWrapper.innerHTML = mainScreen;
-        const goldCoins = Number(useState('gold')) || 0;
-        useMiddleware(() => updateGold(goldCoins));
-        useMiddleware(play);
-    } catch (error) {
-        errorHandler('Main screen', error);
-    }
-}
 
 // Play button functionality
 export const play = () => {
---
+++ controllers/main.js
@@ -0,0 +1,19 @@
+import { mainView } from '../src/views/main.js';
+import { updateGold } from './gold.js';
+import { errorHandler } from '../utils/errorHandler.js';
+import { play } from './play.js';
+import { useState } from '../utils/useState.js';
+import { useMiddleware } from '../utils/useMiddleware.js';
+
+export const main = () => {
+    try {
+        const mainScreen = mainView();
+        const gameWrapper = document.getElementById('game-wrapper');
+        gameWrapper.innerHTML = mainScreen;
+        const goldCoins = Number(useState('gold')) || 0;
+        useMiddleware(() => updateGold(goldCoins));
+        useMiddleware(play);
+    } catch (error) {
+        errorHandler('Main screen', error);
+    }
+}
