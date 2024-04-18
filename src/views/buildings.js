cb4dd62ab8b5270c9b5bc0d0aac0b610452ea220
Naved Uddin <naveduddin83@gmail.com>
2024-04-18 14:10:25 +0530

    buildings view file

---
+++ src/views/buildings.js
@@ -0,0 +1,11 @@
+
+
+export const buildingsView = () => {
+    return `
+    <div class="left-container">
+        <div class="building" id="warrior-building">Warrior</div>
+        <div class="building" id="archer-building">Archer</div>
+        <div class="building" id="cannon-building">Canon</div>
+    </div>
+    `;
+}
