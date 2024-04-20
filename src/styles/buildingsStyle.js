2f6c06182522a696527a4afc2c26c342dba84230
Naved Uddin <naveduddin83@gmail.com>
2024-04-20 17:48:24 +0530

    left side buildings re-design

--- src/styles/buildingsStyle.js
+++ src/styles/buildingsStyle.js
@@ -14,8 +14,16 @@ export const buildingsStyle = () => {
             
             .building {
                 background-color: #ccc;
+
+                background-image: url('https://pics.craiyon.com/2023-10-11/dd028a73d75240198e96e6bb1ecd54ab.webp');
+                background-repeat: no-repeat;
+                background-position: center;
+                background-size: cover;
+                width: 100px;
+                height: 100px;
+
                 border: 1px solid #999;
-                border-radius: 5px;
+                border-radius: 50%;
                 padding: 10px;
                 margin-bottom: 10px;
                 cursor: pointer;
